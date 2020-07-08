bd61f01a8af0f61322c26fa449e9d2ba455841e2
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-07-08 18:30:10 +0100

    Created Props for card element

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div className="bg-light-green dib br3 ma2 grow bw2 shadow-5">
-      <img src="https://robohash.org/test?200x200" alt="robot" />
+    <div className="tc bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
       <div>
-        <h2>John Doe</h2>
-        <p>john.doe@hotmail.co.uk</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );

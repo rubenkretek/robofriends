e1d324de8b38ce382506b669b68ea37b53026ce6
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-07-11 11:14:15 +0100

    Added and styled search field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
 const App = () => {
-  return <CardList robots={robots} />;
+  return (
+    <div className="tc">
+      <h1>Robofriends</h1>
+      <SearchBox />
+      <CardList robots={robots} />;
+    </div>
+  );
 };
 
 export default App;

7fa8f31a2c3a3bbdd1ea32e13f1a61749e33e6ec
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-07-14 18:41:00 +0100

    Cleaned up folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll.js";
+import CardList from "../components/CardList.js";
+import SearchBox from "../components/SearchBox.js";
+import Scroll from "../components/Scroll.js";
 import "./App.css";
 
 class App extends Component {
